785d18bf0befcb9b8c7abe300ec93de9d3a12d84
Gaurang Ingle <148379526+gaurang157@users.noreply.github.com>
2023-12-03 02:01:20 +0530

    Update Ai_Object_Detection.py

--- Ai_Object_Detection.py
+++ Ai_Object_Detection.py
@@ -2,12 +2,12 @@ import requests
 import streamlit as st
 import av
 import logging
-
+# import cv2
 logging.basicConfig(level=logging.WARNING)
 st.set_page_config(page_title="Ai Object Detection", page_icon="🤖")
 from PIL import Image
 from ultralytics import YOLO
-# import cv2
+
 import numpy as np
 from io import BytesIO
 from streamlit_webrtc import (
